3943a215e7592da5f700ea59b3a96d7e2dab98b6
Bahnas <bahnas114@gmail.com>
2026-02-11 23:29:32 +0200

    feat: init onbording screen

--- lib/core/theme/app_images.dart
+++ lib/core/theme/app_images.dart
@@ -1,5 +1,8 @@
 class AppImages {
   static const String authBackground = 'assets/images/BG Asset.png';
   static const String arrowBack = 'assets/svgs/Back.svg';
- 
+  static const String onbording_1 = 'assets/svgs/onbording_1.svg';
+  static const String onbording_2 = 'assets/svgs/onbording_2.svg';
+  static const String onbording_3 = 'assets/svgs/onbording_3.svg';
+  static const String onbording_4 = 'assets/svgs/onbording_4.svg';
 }
