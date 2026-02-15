1e18a13fdb40b2a8cff2d0a227cdb26206bf1352
Bahnas <bahnas114@gmail.com>
2026-02-15 20:57:32 +0200

    feat: update package namespace and integrate Facebook authentication with necessary permissions and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.delivery_app"
+    namespace = "com.app.delivery"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
